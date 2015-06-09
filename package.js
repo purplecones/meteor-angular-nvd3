0f84e1cd838323a1e699915dad425d4cfc92692c
mjoldic <mjoldic@capfundr.com>
2015-06-09 15:17:20 -0400

    updated package.js

--- package.js
+++ package.js
@@ -1,21 +1,16 @@
 Package.describe({
   name: 'purplecones:angular-nvd3',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
+  version: '0.0.2',
   summary: 'An AngularJS directive for NVD3 re-usable charting library (based on D3).',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/purplecones/meteor-angular-nvd3.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.addFiles('nv.d3.js', 'client');
-  api.addFiles('nv.d3.css', 'client')
-  api.addFiles('angular-nvd3.js', 'client')
-  //api.export('Finance');
+  api.addFiles('nv.d3.css', 'client');
+  api.addFiles('angular-nvd3.js', 'client');
 });
 
 Package.onTest(function(api) {
